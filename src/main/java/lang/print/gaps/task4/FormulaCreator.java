2ceb0a2fd86b6d668aaa2c92c859db746a1a2e52
Saidahmad95 <murtazoyev00@gmail.com>
2022-11-02 00:15:48 +0500

    second commit

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -4,7 +4,7 @@ public class FormulaCreator {
     public static void main(String[] args) {
         int a=1;
         int b=2;
-        double v = (9 * Math.sqrt(a) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+        double v = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
 
     }
 }
